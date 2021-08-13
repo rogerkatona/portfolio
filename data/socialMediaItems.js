1ed121f013d958af993282e68c34aaa757211446
Roger Katona <roger.katona@gmail.com>
2021-08-12 21:30:56 -0400

    update social media icons to svgs

--- data/socialMediaItems.js
+++ data/socialMediaItems.js
@@ -1,22 +1,29 @@
 
+
 const socialMediaItems = [
     {
+        label: "Linkedin",
+        src: '/image/icon-linkedIn.svg',
+        path: "https://www.linkedin.com/in/rogerkatona",
+        alt: 'LinkedIn',
+    },
+    {
+        label: "Youtube",
+        src: '/image/icon-Youtube.svg',
+        path: "https://www.youtube.com/channel/UC_wtM1aHa2RUIqm_WQp4JzA",
+        alt: 'Youtube',
+    },
+    {
         label: "Twitter",
-        src: '/image/icon-Twitter.png',
+        src: '/image/icon-Twitter.svg',
         path: "https://twitter.com/roger_katona",
         alt: 'Twitter',
     },
     {
         label: "Instagram",
-        src: '/image/icon-Instagram.png',
+        src: '/image/icon-Instagram.svg',
         path: "https://www.instagram.com/rogerkatona/",
         alt: 'Instagram',
-    },
-    {
-        label: "Linkedin",
-        src: '/image/icon-linkedIn.png',
-        path: "https://www.linkedin.com/in/rogerkatona",
-        alt: 'LinkedIn',
     },
 ];
 
