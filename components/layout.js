60fd2be3536836a24f5c052fd3938b371c296b42
Roger Katona <roger.katona@gmail.com>
2023-04-13 17:38:51 -0400

    Minor edit

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from "./header";
 import Footer from "./footer";
 
-export const siteTitle = 'Roger Katona - UX leadership'
+export const siteTitle = 'Roger Katona'
 
 export default function Layout({ children}) {
 
